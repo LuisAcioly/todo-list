8357707810fe30737f013ed894ac04130667b1d3
LuisAcioly <luiswagner8@gmail.com>
2022-08-16 03:05:55 -0300

    adding models

---
+++ src/models/Task.js
@@ -0,0 +1,8 @@
+export default class Task{
+    constructor(id, descricao, prazo, completa) {
+        this.id = id
+        this.descricao = descricao
+        this.prazo = prazo
+        this.completa = completa
+      }
+}
